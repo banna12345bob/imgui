4a86ded825405645aaa9c8a424e7bbda1f0c6390
banna12345bob <banna12345bob@outlook.com>
2025-04-17 21:05:24 +1000

    Update to SDL3

--- premake5.lua
+++ premake5.lua
@@ -18,34 +18,31 @@ project "ImGui"
 		"imstb_textedit.h",
 		"imstb_truetype.h",
 		"imgui_demo.cpp",
-		"backends/imgui_impl_sdl2.cpp",
-		"backends/imgui_impl_sdl2.h",
-		"backends/imgui_impl_sdlrenderer2.cpp",
-		"backends/imgui_impl_sdlrenderer2.h"
+		"backends/imgui_impl_sdl3.cpp",
+		"backends/imgui_impl_sdl3.h",
+		"backends/imgui_impl_sdlrenderer3.cpp",
+		"backends/imgui_impl_sdlrenderer3.h"
 	}
 
 	includedirs
 	{
 		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.SDL2}"
+		"%{IncludeDir.SDL3}"
 	}
 
 	libdirs
 	{
-		"%{IncludeDir.SDL2}/../lib/x64/"
+		"%{IncludeDir.SDL3}/../lib/x64/"
 	}
 
 	links {
-		"SDL2",
+		"SDL3",
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
 		staticruntime "On"
-		links {
-			"SDL2main"
-		}
 
 	filter "system:linux"
 		pic "On"
